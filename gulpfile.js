b9e0f1e365ce65956377554c12ebdead893d2026
YashkinDmitry <yashkin23@gmail.com>
2019-06-27 23:36:40 +0300

    feature(task-manager): add browser-sync in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpif = require('gulp-if');
+const browserSync = require('browser-sync').create();
 
 const isDev = false;
 const isProd = !isDev;
@@ -23,6 +24,12 @@ var jsFiles = {
     dest: './build/js'
 }
 
+var htmlFiles = {
+    src: './src/*.html',
+    allPreFiles: './src/**/*.html',
+    dest: './build/'
+}
+
 function styles() {
     return gulp.src(cssFiles.src)
         .pipe(gulpif(isDev, sourcemaps.init()))
@@ -37,7 +44,8 @@ function styles() {
             level: 2
         })))
         .pipe(gulpif(isDev, sourcemaps.write()))
-        .pipe(gulp.dest(cssFiles.dest));
+        .pipe(gulp.dest(cssFiles.dest))
+        .pipe(browserSync.stream());
 }
 
 function scripts() {
@@ -46,14 +54,30 @@ function scripts() {
       .pipe(concat('script.js'))
       .pipe(gulpif(isProd, uglify({toplevel: true})))
       .pipe(gulpif(isDev, sourcemaps.write()))
-      .pipe(gulp.dest(jsFiles.dest));
+      .pipe(gulp.dest(jsFiles.dest))
+      .pipe(browserSync.stream());
 }
 
+function html(){
+  return gulp.src(htmlFiles.src)
+      .pipe(gulp.dest(htmlFiles.dest))
+      .pipe(browserSync.stream());
+      // .pipe(browserSync.reload);
+}
 function watch(){
+  browserSync.init({
+    server: {
+      baseDir: "./build/"
+    }
+  });
   gulp.watch(cssFiles.allPreFiles, styles);
   gulp.watch(jsFiles.allPreFiles, scripts);
+  gulp.watch(htmlFiles.allPreFiles, html);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 gulp.task('style', styles);
 gulp.task('script', scripts);
-gulp.task('watch', watch);
+
+let build = gulp.parallel(styles, scripts, html);
+gulp.task('watch', gulp.series(build, watch));

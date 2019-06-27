e1654f4728fd3dba8bfa7db74e96630feadad3db
YashkinDmitry <yashkin23@gmail.com>
2019-06-27 22:04:57 +0300

    feature(task-manager): add mode control for development and production

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,25 @@ const gcmq = require('gulp-group-css-media-queries');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpif = require('gulp-if');
+
+const isDev = false;
+const isProd = !isDev;
 
 var cssFiles = {
-    src: './src/precss/**/*.less',
+    src: './src/precss/style.less',
     dest: './build/css'
 }
 
 var jsFiles = {
-    src: './src/prejs/**/*.js',
+    src: './src/prejs/scripts.js',
     dest: './build/js'
 }
 
 function styles() {
     return gulp.src(cssFiles.src)
+        .pipe(gulpif(isDev, sourcemaps.init()))
         .pipe(less())
         .pipe(concat('style.css'))
         .pipe(gcmq())
@@ -25,16 +31,19 @@ function styles() {
             overrideBrowserslist: ['>0.1%'],
             cascade: false
         }))
-        .pipe(cleanCSS({
+        .pipe(gulpif(isProd, cleanCSS({
             level: 2
-        }))
+        })))
+        .pipe(gulpif(isDev, sourcemaps.write()))
         .pipe(gulp.dest(cssFiles.dest));
 }
 
 function scripts() {
   return gulp.src(jsFiles.src)
+      .pipe(gulpif(isDev, sourcemaps.init()))
       .pipe(concat('script.js'))
-      .pipe(uglify({toplevel: true}))
+      .pipe(gulpif(isProd, uglify({toplevel: true})))
+      .pipe(gulpif(isDev, sourcemaps.write()))
       .pipe(gulp.dest(jsFiles.dest));
 }
 
